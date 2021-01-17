3989d969f93e7b1589105dcb49c1348776dec07d
GalCegla <galcegla@gmail.com>
2021-01-17 20:22:11 +0200

    Create custon 404 page

---
+++ src/components/404-Container.js
@@ -0,0 +1,6 @@
+import * as React from "react";
+import containerStyles from "./404-container.module.css";
+
+export default function Container_404({ children }) {
+  return <div className={containerStyles.container_404}>{children}</div>;
+}
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,14 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from "react";
+import { Link } from "gatsby";
+import Container_404 from "../components/404-Container";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+const Container = Container_404;
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
-const NotFoundPage = () => {
+export default function Not_Found() {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
+    <Container text>
+      <h1>How'd YOU get here?</h1>
+      <Link to="/">Go back?</Link>
+    </Container>
+  );
 }
-
-export default NotFoundPage
