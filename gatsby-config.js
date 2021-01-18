82fde51b7b351705edf75ca27f540e1fb3d16fb7
GalCegla <galcegla@gmail.com>
2021-01-18 18:35:01 +0200

    Add header and animation

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,16 @@
 module.exports = {
-    plugins: [
-
-    ]
-}
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: "src/pages/images/icon.png",
+      },
+    },
+  ],
+};
