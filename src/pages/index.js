5ea12add3a2ab05ee18266712d10381284cb571d
GalCegla <galcegla@gmail.com>
2021-01-20 13:40:14 +0200

    Fix sound issues :)

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,13 @@ import icon from "./images/icon.png";
 import gitHubLogo from "./images/gitHubLogo.png";
 import lottie from "lottie-web";
 import bothAnim from "../animations/bothAnim.json";
+import soundOn from "../animations/soundOn.json";
+import soundOff from "../animations/soundOff.json";
 import useSound from "use-sound";
 import soundOnSfx from "../sfx/soundOn.wav";
 import soundOffSfx from "../sfx/soundOff.wav";
 import clairDeLune from "../sfx/clairDeLune.mp3";
+import { Howl, Howler } from "howler";
 
 const leftLine = styles.leftLine;
 const rightLine = styles.rightLine;
@@ -15,46 +18,59 @@ const logo = styles.logo;
 const box = styles.box;
 const socialIcon = styles.socialIcon;
 const soundAnimationContainer = styles.soundAnimationContainer;
-
 const { useState, useEffect } = React;
+
+const playOn = new Howl({
+  src: [soundOnSfx],
+  volume: 0.25,
+});
+
+const playOff = new Howl({
+  src: [soundOffSfx],
+  volume: 0.25,
+});
+
+const clairDeLuneSfx = new Howl({
+  src: [clairDeLune],
+  volume: 0.75,
+});
 
 export default function IndexPage() {
   const [animationState, setAnimationState] = useState("");
-  const [isOn, setIsOn] = useState(false);
   let animationContainer = React.createRef();
-
-  const [playOn] = useSound(soundOnSfx, { volume: 0.25 });
-  const [playOff] = useSound(soundOffSfx, { volume: 0.25 });
-
-  const [play, { stop, isPlaying }] = useSound(clairDeLune);
+  let animationRef = React.createRef();
 
   useEffect(() => {
-    const animation = lottie.loadAnimation({
+    animationRef.current = lottie.loadAnimation({
       container: animationContainer.current,
       animationData: bothAnim,
       renderer: "svg",
       loop: false,
       autoplay: false,
     });
-    if (animationState === "on") {
-      animation.playSegments([30, 50], true);
-    } else if (animationState === "off") {
-      animation.playSegments([85, 110], true);
-    }
     return () => {
-      animation.destroy();
+      animationRef.current.destroy();
     };
-  });
+  }, [animationContainer, animationRef]);
+
+  useEffect(() => {
+    if (animationState === "on") {
+      animationRef.current.playSegments([30, 50], true);
+    } else if (animationState === "off") {
+      animationRef.current.playSegments([85, 110], true);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [animationState, animationRef]);
 
   const handleClick = () => {
     if (animationState === "off" || animationState === "") {
       setAnimationState("on");
-      playOn();
-      play();
+      playOn.play();
+      clairDeLuneSfx.play();
     } else if (animationState === "on") {
       setAnimationState("off");
-      playOff();
-      stop();
+      playOff.play();
+      clairDeLuneSfx.pause();
     }
   };
 
@@ -64,7 +80,7 @@ export default function IndexPage() {
         className={soundAnimationContainer}
         ref={animationContainer}
         role=""
-        onClick={() => handleClick()}
+        onClick={handleClick}
       />
       <div>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
