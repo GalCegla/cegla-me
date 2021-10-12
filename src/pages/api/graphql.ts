6ef84ada3a3242336505a9510e7668bd863fd00f
Gal ITC <gal@itc.tech>
2021-10-12 11:56:12 +0300

    Add graphql.ts

---
+++ src/pages/api/graphql.ts
@@ -0,0 +1,56 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { ApolloError, ApolloServer } from "apollo-server-micro";
+import { MicroRequest } from "apollo-server-micro/dist/types";
+import { processRequest } from "graphql-upload";
+import { ServerResponse } from "http";
+import schema, { Context } from "schema";
+
+import prisma from "lib/prisma";
+
+/**
+ * The context object passed between Apollo Server resolvers automatically
+ * @see https://www.apollographql.com/docs/apollo-server/api/apollo-server/#middleware-specific-context-fields
+ */
+type ContextFunctionParams = {
+  req: MicroRequest;
+  res: ServerResponse;
+};
+
+const apolloServer = new ApolloServer({
+  uploads: false,
+  context: async ({ req, res }: ContextFunctionParams): Promise<Context> => ({
+    prisma,
+  }),
+  schema,
+  formatError(error) {
+    if (error.originalError) {
+      if (!(error.originalError instanceof ApolloError)) {
+        console.error(error.originalError);
+      }
+    } else {
+      error.stack = error.stack || error?.extensions?.exception?.stacktrace;
+      console.error(error);
+    }
+    return error;
+  },
+});
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const handler = apolloServer.createHandler({ path: "/api/graphql" });
+
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> => {
+  const contentType = req.headers["content-type"];
+  if (contentType && contentType.startsWith("multipart/form-data")) {
+    // @ts-ignore
+    req.filePayload = await processRequest(req, res);
+  }
+  return handler(req, res);
+};
