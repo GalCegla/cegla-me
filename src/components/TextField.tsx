6bc1aac52a446655dc3f53138e28399124cec535
Gal ITC <gal@itc.tech>
2021-10-13 12:36:42 +0300

    Add TextField component

---
+++ src/components/TextField.tsx
@@ -0,0 +1,44 @@
+import React, { FC, useMemo } from "react";
+import {
+  TextField as MuiTextField,
+  TextFieldProps as MuiTextFieldProps,
+} from "@material-ui/core";
+import { FieldConfig, useField } from "formik";
+
+export type TextFieldProps = MuiTextFieldProps &
+  Pick<FieldConfig, "validate"> & {
+    name: string;
+    maxLength?: number;
+    startAdornment?: React.ReactNode;
+  };
+
+/**
+ * Material UI TextField integrated with Formik
+ */
+const TextField: FC<TextFieldProps> = ({
+  maxLength,
+  validate,
+  name,
+  helperText,
+  rows,
+  startAdornment,
+  ...rest
+}) => {
+  const [field, meta] = useField<string>({
+    name,
+    validate,
+  });
+
+  return (
+    <MuiTextField
+      {...rest}
+      {...field}
+      rows={rows}
+      error={Boolean(meta.touched && meta.error)}
+      helperText={meta.touched && meta.error ? meta.error : helperText}
+      inputProps={{ maxLength }}
+    />
+  );
+};
+
+export default TextField;
