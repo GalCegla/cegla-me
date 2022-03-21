14dff5c626985b883e9f948046238a82d5b36bc1
GalCegla <galcegla@gmail.com>
2022-03-21 19:09:49 +0200

    Finish styling HeadPost

--- src/components/HeadPost.tsx
+++ src/components/HeadPost.tsx
@@ -1,28 +1,41 @@
 import styled from "@emotion/styled";
-import { Box } from "@material-ui/core";
+import { Box, Divider, Typography } from "@material-ui/core";
 import DEFAULT_THUMBNAIL from "consts/defaultThumbnail";
 import { FC } from "react";
-import { Post } from "types/post";
+import { getPosts_posts } from "__generated__/getPosts";
 import PostThumbnail, { Size } from "./PostThumbnail";
 
-type HeadPostProps = { post: Post };
+type HeadPostProps = {
+  post: Omit<getPosts_posts, "__typename">;
+};
 
 const HeadPost: FC<HeadPostProps> = ({ post }) => {
+  const createdAt = post.createdAt.slice(0, 10).split("-").reverse().join("/");
+
   return (
     <Container>
-      <PostThumbnail
-        size={Size.LARGE}
-        rating={post.rating}
-        thumbnail={post.thumbnail || DEFAULT_THUMBNAIL}
-      />
-      <HeaderLogo src="/header.png" />
+      <ThumbnailContainer>
+        <PostThumbnail
+          size={Size.LARGE}
+          rating={post.rating}
+          thumbnail={post.thumbnail || DEFAULT_THUMBNAIL}
+        />
+        <HeaderLogo src="/header.png" />
+        <CatchPhrase>Drink it slow</CatchPhrase>
+      </ThumbnailContainer>
+      <CreateAt>
+        <StyledDate variant="body2">{createdAt}</StyledDate>
+        <StyledDivider />
+      </CreateAt>
+      <StyledShopName variant="h3">{post.shop.name}</StyledShopName>
+      <StyledTitle variant="h6">{post.title}</StyledTitle>
     </Container>
   );
 };
 
 export default HeadPost;
 
-const Container = styled(Box)`
+const ThumbnailContainer = styled(Box)`
   padding: 16px;
   border: 1.5px solid #b63719;
   margin: 35px;
@@ -30,6 +43,17 @@ const Container = styled(Box)`
   width: 305px;
   height: 230px;
   position: relative;
+  margin-bottom: 42px;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+`;
+
+const Container = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   margin-bottom: 50px;
 `;
 
@@ -39,3 +63,29 @@ const HeaderLogo = styled.img`
   top: -51px;
   left: -17px;
 `;
+
+const CatchPhrase = styled(Typography)`
+  position: absolute;
+  color: #b63719;
+  font-weight: bold !important;
+  top: -25px;
+  right: 25px;
+`;
+
+const CreateAt = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 15px;
+`;
+
+const StyledDivider = styled(Divider)`
+  background-color: #e76f51 !important;
+`;
+
+const StyledDate = styled(Typography)`
+  letter-spacing: 1.5px !important;
+  margin-bottom: 5px !important;
+`;
+
+const StyledShopName = styled(Typography)``;
+const StyledTitle = styled(Typography)``;
